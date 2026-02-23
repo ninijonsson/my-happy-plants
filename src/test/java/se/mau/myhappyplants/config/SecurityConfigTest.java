00c13d8965a494b792dced7e737501e740a39c7a
AbitVeseli <abit.veseli04@gmail.com>
2026-02-23 20:23:19 +0100

    .

--- src/test/java/se/mau/myhappyplants/config/SecurityConfigTest.java
+++ src/test/java/se/mau/myhappyplants/config/SecurityConfigTest.java
@@ -2,14 +2,34 @@ package se.mau.myhappyplants.config;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SecurityConfigTest {
 
+    private HTTPSecurityConfig config;
+
+
     @BeforeEach
     void setUp() {
+        config = new HTTPSecurityConfig();
     }
+
+    @Test
+    @DisplayName("ACC.01F Login")
+    void testLoginConfiguration() {
+
+    }
+
+
+    @Test
+    @DisplayName("ACC.02F Logout")
+    void testLogoutConfiguration() {
+        assertNotNull(config, "Configuration should not be null");
+    }
+
 
     @AfterEach
     void tearDown() {
