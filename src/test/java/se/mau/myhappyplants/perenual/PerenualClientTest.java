f0e6ca912477210e3c4c5a7d7f2313800509d4a6
Joel <joel.allarp@gmail.com>
2026-03-05 16:04:16 +0100

    fetchPlantById and fetchPlantDetails tests
    Added tests for above functions. Currently we cannot test the actual
    functionality of the api only that it handle information correctly which
    is sad. But I looked into WireMock and it seems way to complicated to
    add this late in the project.

--- src/test/java/se/mau/myhappyplants/perenual/PerenualClientTest.java
+++ src/test/java/se/mau/myhappyplants/perenual/PerenualClientTest.java
@@ -44,6 +44,7 @@ class PerenualClientTest {
     }
 
 
+    //Tests for fetchPlants
     @Test
     void fetchPlants_returnsCorrectlyMappedResults() {
         given(responseSpec.bodyToMono(PerenualSpeciesListResponse.class))
@@ -110,26 +111,100 @@ class PerenualClientTest {
     @Test
     void fetchPlants_limitIs100() {
         var bigList = IntStream.range(0, 150)
-                .mapToObj(i -> makeSpecies(i, "Name " + i, "Scientific Name " + i, null, null, null, null))
+                .mapToObj(i -> makePerenualPlant(i, "Name " + i, "Scientific Name " + i, null, null, null, null))
                 .toList();
         given(responseSpec.bodyToMono(PerenualSpeciesListResponse.class))
                 .willReturn(Mono.just(new PerenualSpeciesListResponse(bigList)));
         
         assertThat(perenualClient.fetchPlants("rose")).hasSize(100);
     }
+    //End of tests for fetchPlants
     
-    // Helper methods to imitate real object
+    //Tests for fetchPlantById
+    @Test
+    void fetchPlantById_returnsCorrectlyMappedResult() {
+        given(responseSpec.bodyToMono(PerenualPlant.class))
+                .willReturn(Mono.just(makePerenualPlant(1, "Rose", "Rosa", "https://img.jpg", "https://thumbnail.jpg", "7", "A flower.")));
+        
+        PlantDetailsView result = perenualClient.fetchPlantById("1");
+        
+        assertThat(result).isNotNull();
+        assertThat(result.name()).isEqualTo("Rose");
+        assertThat(result.scientificName()).isEqualTo("Rosa");
+        assertThat(result.imageUrl()).isEqualTo("https://img.jpg");
+        assertThat(result.wateringFrequency()).isEqualTo("7");
+        assertThat(result.description()).isEqualTo("A flower.");
+    }
+    
+    @Test
+    void fetchPlantById_nullResponse_returnsNull() {
+        given(responseSpec.bodyToMono(PerenualPlant.class))
+                .willReturn(Mono.empty());
+        
+        assertThat(perenualClient.fetchPlantById("1")).isNull();
+    }
+    
+    @Test
+    void fetchPlantById_nameNull() {
+        given(responseSpec.bodyToMono(PerenualPlant.class))
+                .willReturn(Mono.just(makePerenualPlant(1, null, null, null, null, null, null)));
+        assertThat(perenualClient.fetchPlantById("1").name()).isEqualTo(null);
+        assertThat(perenualClient.fetchPlantById("1").scientificName()).isEqualTo(null);
+    }
+    
+    @Test
+    void fetchPlantById_nullImage() {
+        given(responseSpec.bodyToMono(PerenualPlant.class))
+                .willReturn(Mono.just(makePerenualPlant(1, "Rose", "Rosa", null, null, null, null)));
+        assertThat(perenualClient.fetchPlantById("1").imageUrl()).isEqualTo(null);
+    }
+    
+    @Test
+    void fetchPlantById_nullWateringFrequency() {
+        given(responseSpec.bodyToMono(PerenualPlant.class))
+                .willReturn(Mono.just(makePerenualPlant(1, "Rose", "Rosa", "https://img.jpg", null, null, null)));
+        assertThat(perenualClient.fetchPlantById("1").wateringFrequency()).isEqualTo(null);
+    }
+    // End of tests for fetchPlantById
+    
+    //Tests for fetchPlantDetails
+    @Test
+    void fetchPlantDetails_ReturnsCorrectlyMappedResult() {
+        given(responseSpec.bodyToMono(PerenualPlantDetailsResponse.class))
+                .willReturn(Mono.just(makePerenualPlantDetailsResponse(1, "Rose", "Rosa", 
+                                                                "Somthing Fancy", "7", 
+                                                                "More than once", "A flower.", 
+                                                                "No can do", "Once every 10 000 km", 
+                                                                "https://img.jpg", "https://thumbnail.jpg")));
 
+        PerenualPlantDetailsResponse result = perenualClient.fetchPlantDetails("1");
+
+        assertThat(result).isNotNull();
+        assertThat(result.getId()).isEqualTo(1);
+        assertThat(result.getCommonName()).isEqualTo("Rose");
+        assertThat(result.getScientificName()).contains("Rosa");
+        assertThat(result.getFamily()).isEqualTo("Somthing Fancy");
+        assertThat(result.getSunlight()).contains("More than once");
+        assertThat(result.getCycle()).isEqualTo("No can do");
+        assertThat(result.getMaintenance()).isEqualTo("Once every 10 000 km");
+        assertThat(result.getWatering()).isEqualTo("7");
+        assertThat(result.getDescription()).isEqualTo("A flower.");
+        assertThat(result.getDefaultImage().regularUrl()).isEqualTo("https://img.jpg");
+        assertThat(result.getDefaultImage().thumbnail()).isEqualTo("https://thumbnail.jpg");
+    }
+    //End of tests for fetchPlantDetails
+    
+    // Helper methods to imitate the real object
     private PerenualSpeciesListResponse responseWithPlant(String name, String scientificName,
                                                           String imageUrl, String thumbnail, String wateringDays,
                                                           String description) {
         return new PerenualSpeciesListResponse(
-                List.of(makeSpecies(1, name, scientificName, imageUrl, thumbnail, wateringDays, description))
+                List.of(makePerenualPlant(1, name, scientificName, imageUrl, thumbnail, wateringDays, description))
         );
     }
 
-    private PerenualPlant makeSpecies(int id, String name, String scientificName,
-                                        String imageUrl, String thumbnail, String wateringDays, String description) {
+    private PerenualPlant makePerenualPlant(int id, String name, String scientificName,
+                                            String imageUrl, String thumbnail, String wateringDays, String description) {
         return new PerenualPlant(
                 id,
                 name,
@@ -140,6 +215,20 @@ class PerenualClientTest {
         );
     }
     
-    
+    private PerenualPlantDetailsResponse makePerenualPlantDetailsResponse(int id, String commonName, String scientificName, String family,
+                                                                          String watering, String sunlight, String description, String cycle, String maintenance, String imageUrl, String thumbnail) {
+        PerenualPlantDetailsResponse response = new PerenualPlantDetailsResponse();
+        response.setId(id);
+        response.setCommonName(commonName);
+        response.setScientificName(List.of(scientificName));
+        response.setFamily(family);
+        response.setWatering(watering);
+        response.setSunlight(List.of(sunlight));
+        response.setDescription(description);
+        response.setCycle(cycle);
+        response.setMaintenance(maintenance);
+        response.setDefaultImage(new PerenualPlant.DefaultImage(thumbnail, imageUrl));
+        return response;
+    }
     
 }
