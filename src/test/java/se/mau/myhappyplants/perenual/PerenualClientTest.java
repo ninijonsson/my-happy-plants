804b36fb5a6de2f794446003f7f8d90af62d44f4
saraSheikho <sarasheikho885@gmail.com>
2026-03-17 04:46:56 +0100

    PerenualClient class unit tests(for higher coverage)

--- src/test/java/se/mau/myhappyplants/perenual/PerenualClientTest.java
+++ src/test/java/se/mau/myhappyplants/perenual/PerenualClientTest.java
@@ -2,6 +2,7 @@ package se.mau.myhappyplants.perenual;
 
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,6 +10,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 import se.mau.myhappyplants.perenual.dto.PerenualPlant;
 import se.mau.myhappyplants.perenual.dto.PerenualSpeciesListResponse;
@@ -19,6 +22,7 @@ import java.util.function.Function;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
@@ -39,16 +43,22 @@ class PerenualClientTest {
     @BeforeEach
     void setUp() {
         given(webClient.get()).willReturn(uriSpec);
-        given(uriSpec.uri(any(Function.class))).willReturn(headersSpec);
+        given(uriSpec.uri(any(Function.class))).willAnswer(invocation -> {
+            Function<UriBuilder, java.net.URI> uriBuilderFunction = invocation.getArgument(0);
+            uriBuilderFunction.apply(UriComponentsBuilder.newInstance());  //To execute the lambda expression
+            return headersSpec;
+        });
         given(headersSpec.retrieve()).willReturn(responseSpec);
     }
 
 
     //Tests for fetchPlants
     @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
     void fetchPlants_returnsCorrectlyMappedResults() {
         given(responseSpec.bodyToMono(PerenualSpeciesListResponse.class))
-                .willReturn(Mono.just(responseWithPlant("Rose", "Rosa", "https://img.jpg", "https://thumbnail.jpg", "7", "A flower.")));
+                .willReturn(Mono.just(responseWithPlant("Rose", "Rosa", "https://img.jpg",
+                        "thumb.jpg", "7", "A flower.")));
 
         List<PlantDetailsView> result = perenualClient.fetchPlants("rose");
 
@@ -61,6 +71,7 @@ class PerenualClientTest {
     }
     
     @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
     void fetchPlants_nameNull_useDefaultLable() {
         given(responseSpec.bodyToMono(PerenualSpeciesListResponse.class))
                 .willReturn(Mono.just(responseWithPlant(null, null, null, null, null, null)));
@@ -69,6 +80,7 @@ class PerenualClientTest {
     }
     
     @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
     void fetchPlants_nullImage_useDefaultImage() {
         given(responseSpec.bodyToMono(PerenualSpeciesListResponse.class))
                 .willReturn(Mono.just(responseWithPlant("Rose", "Rosa", null, null, null, null)));
@@ -77,6 +89,7 @@ class PerenualClientTest {
     }
     
     @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
     void fetchPlants_nullWateringFrequency_defaultToZero() {
         given(responseSpec.bodyToMono(PerenualSpeciesListResponse.class))
                 .willReturn(Mono.just(responseWithPlant("Rose", "Rosa", "https://img.jpg", "https://thumbnail.jpg", null, null)));
@@ -85,6 +98,7 @@ class PerenualClientTest {
     }
     
     @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
     void fetchPlants_nullResponse_returnsEmptyList() {
         given(responseSpec.bodyToMono(PerenualSpeciesListResponse.class))
                 .willReturn(Mono.empty());
@@ -93,6 +107,7 @@ class PerenualClientTest {
     }
     
     @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
     void fetchPlants_apiThrows_returnsEmptyList() {
         given(responseSpec.bodyToMono(PerenualSpeciesListResponse.class))
                 .willReturn(Mono.error(new RuntimeException("API down")));
@@ -101,6 +116,7 @@ class PerenualClientTest {
     }
 
     @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
     void fetchPlants_tooManyRequests_returnsEmptyList() {
         given(responseSpec.bodyToMono(PerenualSpeciesListResponse.class))
                 .willReturn(Mono.error(WebClientResponseException.create(429, "Too Many Requests", null, null, null)));
@@ -109,6 +125,7 @@ class PerenualClientTest {
     }
     
     @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
     void fetchPlants_limitIs100() {
         var bigList = IntStream.range(0, 150)
                 .mapToObj(i -> makePerenualPlant(i, "Name " + i, "Scientific Name " + i, null, null, null, null))
@@ -118,10 +135,82 @@ class PerenualClientTest {
         
         assertThat(perenualClient.fetchPlants("rose")).hasSize(100);
     }
+
+    @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
+    void fetchPlants_nullQuery(){
+        given(responseSpec.bodyToMono(PerenualSpeciesListResponse.class))
+                .willReturn(Mono.just(new PerenualSpeciesListResponse(null)));
+        assertThat(perenualClient.fetchPlants(null)).isEmpty();
+    }
+
+    @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
+    void fetchPlants_blankQuery(){
+        given(responseSpec.bodyToMono(PerenualSpeciesListResponse.class))
+                .willReturn(Mono.just(new PerenualSpeciesListResponse(List.of())));
+        assertThat(perenualClient.fetchPlants("  ")).isEmpty();
+    }
+
+    @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
+    void fetchPlants_blankCommonName(){
+        given(responseSpec.bodyToMono(PerenualSpeciesListResponse.class))
+                .willReturn(Mono.just(responseWithPlant("  ", "Rosa", "https://imge.jpg",
+                        "thumb.jpg", "7", "description")));
+        assertThat(perenualClient.fetchPlants("rose").get(0).name()).isEqualTo("(no common name)");
+    }
+
+    @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
+    void fetchPlants_blankWateringFrequency() {
+        given(responseSpec.bodyToMono(PerenualSpeciesListResponse.class))
+                .willReturn(Mono.just(responseWithPlant("Rose", "Rosa", "https://img.jpg",
+                        "thumb.jpg", "   ", "desc")));
+
+        assertThat(perenualClient.fetchPlants("rose").get(0).wateringFrequency()).isEqualTo("0");
+    }
+
+    @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
+    void fetchPlants_blankDescription() {
+        given(responseSpec.bodyToMono(PerenualSpeciesListResponse.class))
+                .willReturn(Mono.just(responseWithPlant("Rose", "Rosa", "https://img.jpg",
+                        "thumb.jpg", "7", "   ")));
+
+        assertThat(perenualClient.fetchPlants("rose").get(0).description()).isNull();
+    }
+
+    @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
+    void fetchPlants_nullScientificNameList() {
+        PerenualPlant plant = new PerenualPlant(
+                1, "Rose", null,
+                new PerenualPlant.DefaultImage("thumb.jpg", "https://img.jpg"),
+                new PerenualPlant.WateringFrequency("7"),
+                "description");
+        given(responseSpec.bodyToMono(PerenualSpeciesListResponse.class))
+                .willReturn(Mono.just(new PerenualSpeciesListResponse(List.of(plant))));
+        assertThat(perenualClient.fetchPlants("rose").get(0).scientificName()).isNull();
+    }
+
+    @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
+    void fetchPlants_nullWateringFrequencyDays() {
+        PerenualPlant plant = new PerenualPlant(1, "Rose", List.of("Rosa"),
+                new PerenualPlant.DefaultImage("thumb.jpg", "https://imge.jpg"),
+                new PerenualPlant.WateringFrequency(null), "description");
+
+        given(responseSpec.bodyToMono(PerenualSpeciesListResponse.class))
+                .willReturn(Mono.just(new PerenualSpeciesListResponse(List.of(plant))));
+        assertThat(perenualClient.fetchPlants("rose").get(0).wateringFrequency()).isEqualTo("0");
+    }
+
     //End of tests for fetchPlants
     
     //Tests for fetchPlantById
     @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
     void fetchPlantById_returnsCorrectlyMappedResult() {
         given(responseSpec.bodyToMono(PerenualPlant.class))
                 .willReturn(Mono.just(makePerenualPlant(1, "Rose", "Rosa", "https://img.jpg", "https://thumbnail.jpg", "7", "A flower.")));
@@ -137,6 +226,7 @@ class PerenualClientTest {
     }
     
     @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
     void fetchPlantById_nullResponse_returnsNull() {
         given(responseSpec.bodyToMono(PerenualPlant.class))
                 .willReturn(Mono.empty());
@@ -145,6 +235,7 @@ class PerenualClientTest {
     }
     
     @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
     void fetchPlantById_nameNull() {
         given(responseSpec.bodyToMono(PerenualPlant.class))
                 .willReturn(Mono.just(makePerenualPlant(1, null, null, null, null, null, null)));
@@ -153,6 +244,7 @@ class PerenualClientTest {
     }
     
     @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
     void fetchPlantById_nullImage() {
         given(responseSpec.bodyToMono(PerenualPlant.class))
                 .willReturn(Mono.just(makePerenualPlant(1, "Rose", "Rosa", null, null, null, null)));
@@ -160,15 +252,49 @@ class PerenualClientTest {
     }
     
     @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
     void fetchPlantById_nullWateringFrequency() {
         given(responseSpec.bodyToMono(PerenualPlant.class))
                 .willReturn(Mono.just(makePerenualPlant(1, "Rose", "Rosa", "https://img.jpg", null, null, null)));
         assertThat(perenualClient.fetchPlantById("1").wateringFrequency()).isEqualTo(null);
     }
+
+    @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
+    void fetchPlantById_wateringFrequencyDaysNull() {
+        PerenualPlant plant = new PerenualPlant(1, "Rose", List.of("Rosa"),
+                new PerenualPlant.DefaultImage("thumb.jpg", "https://imge.jpg"),
+                new PerenualPlant.WateringFrequency(null), "description");
+        given(responseSpec.bodyToMono(PerenualPlant.class)).willReturn(Mono.just(plant));
+        assertThat(perenualClient.fetchPlantById("1").wateringFrequency()).isNull();
+    }
+
+    @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
+    void fetchPlantById_emptyScientificNameList() {
+        PerenualPlant plant = new PerenualPlant(1, "Rose", List.of(),
+                new PerenualPlant.DefaultImage("thumb.jpg", "https://imge.jpg"),
+                new PerenualPlant.WateringFrequency("7"), "description");
+        given(responseSpec.bodyToMono(PerenualPlant.class)).willReturn(Mono.just(plant));
+        assertThat(perenualClient.fetchPlantById("1").scientificName()).isNull();
+    }
+
+    @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
+    void fetchPlantById_nullScientificNameList() {
+        PerenualPlant plant = new PerenualPlant(1, "Rose", null,
+                new PerenualPlant.DefaultImage("thumb.jpg", "https://img.jpg"),
+                new PerenualPlant.WateringFrequency("7"), "description");
+
+        given(responseSpec.bodyToMono(PerenualPlant.class)).willReturn(Mono.just(plant));
+        assertThat(perenualClient.fetchPlantById("1").scientificName()).isNull();
+    }
+
     // End of tests for fetchPlantById
     
     //Tests for fetchPlantDetails
     @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
     void fetchPlantDetails_ReturnsCorrectlyMappedResult() {
         given(responseSpec.bodyToMono(PerenualPlantDetailsResponse.class))
                 .willReturn(Mono.just(makePerenualPlantDetailsResponse(1, "Rose", "Rosa", 
@@ -191,6 +317,24 @@ class PerenualClientTest {
         assertThat(result.getDescription()).isEqualTo("A flower.");
         assertThat(result.getDefaultImage().regularUrl()).isEqualTo("https://img.jpg");
         assertThat(result.getDefaultImage().thumbnail()).isEqualTo("https://thumbnail.jpg");
+    }
+
+    @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
+    void fetchPlantDetailsNullResponse(){
+        given(responseSpec.bodyToMono(PerenualPlantDetailsResponse.class))
+                .willReturn(Mono.empty());
+        assertThat(perenualClient.fetchPlantDetails("1")).isNull();
+    }
+
+    @Test
+    @DisplayName("SEA.01.5F-Third Party API-")
+    void fetchPlantDetails_ThrowingPropagatesException(){
+        given(responseSpec.bodyToMono(PerenualPlantDetailsResponse.class))
+                .willReturn(Mono.error(new Exception("API down")));
+        assertThatThrownBy(() -> perenualClient.fetchPlantDetails("1"))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("API down");
     }
     //End of tests for fetchPlantDetails
     
