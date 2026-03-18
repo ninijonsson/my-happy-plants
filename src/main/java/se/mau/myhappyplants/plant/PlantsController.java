6e4bf6c6bb6784425e3c75f8edc1ce1cd08cdfc3
Joel Seger <joel.allarp@gmail.com>
2026-03-18 14:41:35 +0100

    Change prepareDetails method to public access modifier

--- src/main/java/se/mau/myhappyplants/plant/PlantsController.java
+++ src/main/java/se/mau/myhappyplants/plant/PlantsController.java
@@ -70,7 +70,7 @@ public class PlantsController {
      * to use it for the library plants and
      * the search plants
      */
-    private String prepareDetails(String apiId, AccountUserPlant plant, Model model, AccountUser user) {
+    public String prepareDetails(String apiId, AccountUserPlant plant, Model model, AccountUser user) {
       
         //TODO: Add error handling if the plant is null by viewing error message and redirect to the library
 
